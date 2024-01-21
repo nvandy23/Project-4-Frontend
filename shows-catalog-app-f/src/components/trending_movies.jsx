5ab388529e5c9131abd8f67a34d82afbac279232
nvandy23 <nvandy2373@gmail.com>
2024-01-21 10:21:42 -0500

    add conditional rendering of save button ontrending_movies page.

--- shows-catalog-app-f/src/components/trending_movies.jsx
+++ shows-catalog-app-f/src/components/trending_movies.jsx
@@ -54,7 +54,9 @@ const TrendingMoviesPage = () => {
           <p>Movie Description: {movie.overview}</p>
           <p>Movie release date: {movie.release_date}</p>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-          <button onClick={() => handleSaveFavorite(movie)}>Save</button>
+          {isAuthenticated && user && (
+            <button onClick={() => handleSaveFavorite(movie)}>Save</button>
+          )}
         </div>
       ))}
 
